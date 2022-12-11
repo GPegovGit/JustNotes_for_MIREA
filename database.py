1ca2c97380fbe32460a3cc1a0433549a4ade6214
kir270202 <kir.27.artemov@gmail.com>
2022-12-12 01:15:38 +0300

    added getters

--- database.py
+++ database.py
@@ -351,6 +351,51 @@ def filter_employee(first_name: str = None, second_name: str = None, patronymyc:
                         print("[INFO] Error. Employee filter error. Reason: ", _ex)
 
 
+# Получение должности по id сотрудника
+def get_job_title_by_employee_id(employee_id: int):
+        with connection.cursor() as cursor:
+                try:
+                        query_str = f'SELECT job_title FROM public.employee WHERE employee_id = {employee_id}'
+                        cursor.execute(query_str)
+                        filtered_employees = cursor.fetchall()
+
+                        print(f'role of employee: {filtered_employees[0]}')
+
+                except Exception as _ex:
+                        print("[INFO] Error.get_job_title_by_employee_id error. Reason: ", _ex)
+
+
+# Получение должности по id сотрудника
+def get_job_title_by_employee_id(employee_id: int):
+        with connection.cursor() as cursor:
+                try:
+                        query_str = f'SELECT job_title FROM public.employee WHERE employee_id = {employee_id}'
+                        cursor.execute(query_str)
+                        filtered_employees = cursor.fetchall()
+
+                        print(f'role of an employee: {filtered_employees[0]}')
+
+                except Exception as _ex:
+                        print("[INFO] Error.get_job_title_by_employee_id error. Reason: ", _ex)
+
+
+# Получение должности по id сотрудника
+def get_job_title_and_id_by_username(username: int):
+        with connection.cursor() as cursor:
+                try:
+                        query_str = f'SELECT job_title, employee_id FROM public.employee WHERE username = \'{username}\''
+                        cursor.execute(query_str)
+                        role_and_id = cursor.fetchall()
+
+                        for row in role_and_id:
+                                print("job_title = ", row[0], )
+                                print("employee_id = ", row[1], "\n")
+
+                except Exception as _ex:
+                                print("[INFO] get_job_title_and_id_by_username error. Reason: ", _ex)
+
+
+
 
 try:
         # connect to exist database
@@ -383,6 +428,8 @@ try:
         #filter_tasks(1)
         #filter_client('Company_1', city='Chicago')
         #filter_employee(role='manager', first_name='Дмитрий')
+        #get_job_title_by_employee_id(1)
+        #get_job_title_and_id_by_username('manager_1')
 
 
 
